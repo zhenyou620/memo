fdea6760e0c6ae3d4711b8e690f74c3f2125a181
zhenyou620 <bankouzhenyou8@gmail.com>
2024-07-17 14:13:15 +0900

    Loading書き換え

--- reactapp1.client/src/fetures/TaskBox/components/TaskList/Loading.tsx
+++ reactapp1.client/src/fetures/TaskBox/components/TaskList/Loading.tsx
@@ -1,6 +1,6 @@
 import { FC } from 'react';
 
-export const LoadingRow = (
+const LoadingRow = (
   <div className="flex w-full items-center p-4">
     <div className="peer mr-4 h-4 w-4 shrink-0 animate-pulse rounded-sm bg-gray-200 py-2"></div>
     <div className="mr-3 h-6 w-32 animate-pulse bg-gray-200"></div>
@@ -12,7 +12,7 @@ export const LoadingRow = (
 
 export const Loading: FC = () => {
   return (
-    <div role="status" aria-live="polite" aria-busy="true">
+    <div>
       {LoadingRow}
       {LoadingRow}
       {LoadingRow}
